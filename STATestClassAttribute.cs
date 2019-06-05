feba3c5eb7fa0396f5fbc16048273ae46a7dafe4
benzhai <ben.zhai@grapecity.com>
2019-06-05 11:45:42 +0800

    implement STA Attribute.

---
+++ STATestClassAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSTest.STATestMethod
+{
+    public class STATestClassAttribute : TestClassAttribute
+    {
+        public override TestMethodAttribute GetTestMethodAttribute(TestMethodAttribute testMethodAttribute)
+        {
+            if (testMethodAttribute is STATestMethodAttribute)
+                return testMethodAttribute;
+
+            return new STATestMethodAttribute(base.GetTestMethodAttribute(testMethodAttribute));
+        }
+    }
+}
